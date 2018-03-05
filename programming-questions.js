4a8dfed82aaa8006020bf65367cae4b7b9c762ac
Kwena Mokgohlwa <kwena@Kwenas-MacBook-Air.local>
2018-03-06 00:18:35 +0200

    Assignment Clean Coding Complete

---
+++ programming-questions.js
@@ -0,0 +1,48 @@
+function square(integerArray){
+	var total = 0;
+	for(i=0; i < integerArray.length; i++){
+		total += integerArray[i];
+	}
+
+	if(Math.sqrt(total) % 1 === 0){
+		 return Math.sqrt(total);
+	}else {
+		return total;
+	}
+}
+
+function duplicate(integerArray){
+	var checkingValue = null;
+
+	for (var i = 0; i < integerArray.length; i++) {
+		checkingValue = integerArray[i];
+		for (var j = i+1; j < integerArray.length; j++) {
+			if(checkingValue === integerArray[j]){
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+function anagram(stringOne, stringTwo){
+	var arrayOne = stringOne.split('');
+	var arrayTwo = stringTwo.split('');
+
+	var isAnagram = false;
+
+	for (var i = 0; i < arrayOne.length; i++) {
+		isAnagram = false;
+		for (var j = 0; j < arrayTwo.length; j++) {
+			if(arrayOne[i] === arrayTwo[j]){
+				isAnagram = true;
+			}
+		}
+		if(isAnagram === false){
+			return false
+		}
+	}
+
+	return true;
+}
