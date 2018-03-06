3a951e13e02f64a2786721024d0b148c7f8b2aa1
Kwena Mokgohlwa <kwena@Kwenas-MacBook-Air.local>
2018-03-06 20:32:07 +0200

    Assignment Hash Tables Complete

--- programming-questions.js
+++ programming-questions.js
@@ -56,3 +56,51 @@ function searchPhoneBook(entry){
 	}
 	return "Entry not found";
 }
+
+//Hash Tables: Question 6
+var myHash = [{}];
+
+function insertHash(name, address, phoneNumber){
+	myHash.push({phoneNumber: phoneNumber, name: name, address: address});
+}
+
+function retrieveHash(entry){
+	for(i = 0; i < myHash.length; i++){
+		if(myHash[i].phoneNumber === entry){
+			return myHash[i];
+		}
+	}
+	return "Entry not found"
+}
+
+//Hash Tables: Question 7
+var myHash = [{}];
+
+function insertHash(code, name, price, stock){
+	myHash.push({code: code, name: name, price: price, stock: stock});
+}
+
+function retrieveHash(code){
+	for(i = 0; i < myHash.length; i++){
+		if(myHash[i].code === code){
+			return myHash[i];
+		}
+	}
+	return "Inventory item not found"
+}
+
+//Hash Tables: Question 8
+var myHash = [{}];
+
+function insertHash(name, publisher, publicationDate){
+	myHash.push({name: name, publisher: publisher, publicationDate: publicationDate});
+}
+
+function retrieveHash(publication, publicationDate){
+	for(i = 0; i < myHash.length; i++){
+		if(myHash[i].publication === publication && myHash[i].publicationDate === publicationDate){
+			return myHash[i];
+		}
+	}
+	return "Newspaper not found"
+}
